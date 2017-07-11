015fb7795fd9384d61f8b07b573ba017d2bd356b
rznazn <sportrider84@gmail.com>
2017-07-11 13:41:08 -0500

    initial commit

---
+++ app/src/main/java/com/babykangaroo/android/gpsdataloggerv2/FileManagerActivity.java
@@ -0,0 +1,13 @@
+package com.babykangaroo.android.gpsdataloggerv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FileManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_manager);
+    }
+}
