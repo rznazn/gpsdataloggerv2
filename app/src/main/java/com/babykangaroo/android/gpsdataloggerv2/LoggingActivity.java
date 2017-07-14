d130e203b2d5a6e1b8d76265fd42cd211a053378
rznazn <sportrider84@gmail.com>
2017-07-14 10:38:52 -0500

    adjust to store a formatted date

--- app/src/main/java/com/babykangaroo/android/gpsdataloggerv2/LoggingActivity.java
+++ app/src/main/java/com/babykangaroo/android/gpsdataloggerv2/LoggingActivity.java
@@ -7,12 +7,15 @@ import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.babykangaroo.android.mydatabaselibrary.ListContract;
 import com.babykangaroo.android.mylocationlibrary.LocationAccess;
+
+import java.text.ParseException;
 
 public class LoggingActivity extends AppCompatActivity implements LocationAccess.LocationUpdateListener{
 
@@ -23,6 +26,7 @@ public class LoggingActivity extends AppCompatActivity implements LocationAccess
     private Context context;
 
     private String mCurrentLog;
+    private long mTimeCorrection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +47,9 @@ public class LoggingActivity extends AppCompatActivity implements LocationAccess
         tvLogEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mContentValues !=null) {
+                if(mContentValues.size() == 0) {
                     Uri uri = getContentResolver().insert(ListContract.ListContractEntry.ITEMS_CONTENT_URI, mContentValues);
-                    Toast.makeText(context, uri.toString(), Toast.LENGTH_LONG).show();
+                    Log.v("LOGGING ACTIVITY", "LOGGED");
                 }else {
                     Toast.makeText(context, "content values still null", Toast.LENGTH_LONG).show();
                 }
@@ -55,14 +59,23 @@ public class LoggingActivity extends AppCompatActivity implements LocationAccess
     }
 
     @Override
-    public void onLocationUpdate(Location location) {
-        mContentValues.put(ListContract.ListContractEntry.COLUMN_EVENT_TIME, System.currentTimeMillis());
+    public void onLocationUpdate(Location location){
+        long time = System.currentTimeMillis();
+        mTimeCorrection = mLocationAccess.getmGPSTimeOffset();
+        String eventTime = "";
+        java.text.SimpleDateFormat dateFormat = new java.text.SimpleDateFormat("yyyyMMdd\\HHmmss\\SSS");
+        try {
+            eventTime = dateFormat.parse(String.valueOf(time - mTimeCorrection)).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        mContentValues.put(ListContract.ListContractEntry.COLUMN_EVENT_TIME, eventTime);
         mContentValues.put(ListContract.ListContractEntry.COLUMN_ITEM_PARENT_LIST, mCurrentLog);
         mContentValues.put(ListContract.ListContractEntry.COLUMN_EVENT_LATITUDE, location.getLatitude());
         mContentValues.put(ListContract.ListContractEntry.COLUMN_EVENT_LONGITUDE, location.getLongitude());
         mContentValues.put(ListContract.ListContractEntry.COLUMN_EVENT_ALTITUDE, location.getAltitude());
         mContentValues.put(ListContract.ListContractEntry.COLUMN_EVENT_BEARING_FROM_LAST, location.getBearing());
         mContentValues.put(ListContract.ListContractEntry.COLUMN_SPEED_FROM_LAST, location.getSpeed());
-
+        Uri uri = getContentResolver().insert(ListContract.ListContractEntry.ITEMS_CONTENT_URI, mContentValues);
     }
 }
